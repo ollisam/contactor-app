2f460d99f2f906fd9b1a7b1740fbb40793ae57e1
ollisam <olivers23@ru.is>
2025-12-04 13:38:15 +0000

    search bar modifications

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -7,14 +7,14 @@ const search = () => {
         <View className="flex-1 bg-background px-6 pt-20">
             {/* Header row */}
             <ScrollView className="flex-1 px-1" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight: "100%", paddingBottom: 10}}>
-                <View className="flex-row items-center justify-between mt-16 mb-2.5">
+                <View className="flex-row items-center justify-between mt-16 mb-3">
                     <Text className="text-6xl text-primary font-bold font-instrument" style={{ lineHeight: 55}}>
                         Recents
                     </Text>
                 </View>
 
                 {/* Search bar */}
-                <View className="flex-1">
+                <View className="w-full">
                     <SearchBar
                         onPress={() => {}}
                         placeholder="Search"
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -12,7 +12,7 @@ interface Props {
 const SearchBar = ({ placeholder, onChangeText, value }: Props) => {
     return (
         <View
-            className="w-full h-7 bg-grey-200 rounded-full flex-row items-center">
+            className="w-full h-10 bg-grey-200 rounded-full flex-row items-center">
             <Octicons name="search" size={15} color="#5D5C5C" className="px-3"/>
             <TextInput
                 className="flex-1 text-sm text-primary font-worksans"
